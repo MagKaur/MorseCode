081746ed88325820f060107cba11638620f59078
MagKaur <kochanowska.magd@gmail.com>
2021-03-23 20:38:33 +0100

    without nnumber check if not char

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+
+class StringToMorseCode {
+    public static void main(String[] args) {
+        MorseConverter converter = new MorseConverter("","","");
+        converter.getNumber();
+        converter.translateToMorse();
+        converter.show();
+    }
+}
---
+++ src/main/java/MorseConverter.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MorseConverter {
+    Scanner sc = new Scanner(System.in);
+    private String numberToChange;
+    private String translated;
+    private String morseCode;
+
+
+    char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+    String[] morseLetters = {". ___ ___ ___ ___", ". . ___ ___ ___", ". . . ___ ___", ". . . . ___", ". . . . .", "___ . . . .", "___ ___ . . .",
+            "___ ___ ___ . .", "___ ___ ___ ___ .", "___ ___ ___ ___ ___"};
+
+    public MorseConverter(String textToChange, String translatedText, String morseCode) {
+        this.numberToChange = textToChange;
+        this.translated = translatedText;
+        this.morseCode = morseCode;
+    }
+    public void getNumber() {
+        System.out.println("Enter numbers: ");
+        numberToChange = sc.nextLine();
+    }
+
+    public void translateToMorse() {
+        for (int i = 0; i < numberToChange.length(); i++) {
+            for (short j = 0; j < numbers.length; j++) {
+               if (numberToChange.charAt(i) == numbers[j]) {
+                    translated += morseLetters[j];
+                    String result[] = {translated};
+                    morseCode = Arrays.toString(result);
+                }
+            }
+        }
+    }
+    public void show (){
+        System.out.println(morseCode);
+    }
+}
+
